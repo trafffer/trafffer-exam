490d90ba2f4724461c8396233510ad7b3461ea37
trafffer <sonya0@mail.bg>
2023-12-22 12:38:24 +0200

    Fixed UI tests urls

--- tests/ui/about.test.js
+++ tests/ui/about.test.js
@@ -1,14 +1,14 @@
 const { test, expect } = require('@playwright/test');
 
 test('Check about page', async ({ page }) => {
-  await page.goto('http://localhost:8090/About');
+  await page.goto('https://trafffer-exam.onrender.com/About');
   const heading = await page.$('h1');
   const text = await heading.textContent();
   expect(text).toBe('About');
 });
 
 test('Check about page test', async ({ page }) => {
-  await page.goto('http://localhost:8090/About');
+  await page.goto('https://trafffer-exam.onrender.com/About');
   const heading = await page.$('a');
   const text = await heading.textContent();
   expect(text).toBe('Home');
